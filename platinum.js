00b792ea9c7705b7401d289790ab81d1df11ee25
Sergio Santana <src.santana@gmail.com>
2022-04-25 13:01:01 +0200

    added all marks for obstacles

--- platinum.js
+++ platinum.js
@@ -605,11 +605,11 @@ var platinum = {
     // if null, then we don't do tracking
     // if undefined, then we don't draw the mark
     marks: [
-        [["unknown", 0], ["dead_end", null], ["event",  0], ["center", 0], ["mart",     0], ["one_way", null]],
-        [["normal",  0], ["fighting",    0], ["flying", 0], ["poison", 0], ["ground",   0], ["rock",       0], ["bug",  0], ["ghost",   0], ["fairy", 0]],
-        [["steel",   0], ["fire",        0], ["water",  0], ["grass",  0], ["electric", 0], ["psychic",    0], ["ice",  0], ["dragon",  0], ["dark",  0]],
-        [["1",       0], ["2",           0], ["3",      0], ["4",      0], ["5",        0], ["6",          0], ["7",    0], ["8",       0]],
-        [["e_rock",  0], ["e_boulder",   0], ["e_surf", 0], ["e_dive", 0], ["e_bike",   0], ["e_trainer",  0]],
+        [["unknown", 0], ["dead_end", null], ["event",     0], ["center", 0], ["mart",        0], ["one_way",  null]],
+        [["normal",  0], ["fighting",    0], ["flying",    0], ["poison", 0], ["ground",      0], ["rock",        0], ["bug",         0], ["ghost",      0], ["fairy", 0]],
+        [["steel",   0], ["fire",        0], ["water",     0], ["grass",  0], ["electric",    0], ["psychic",     0], ["ice",         0], ["dragon",     0], ["dark",  0]],
+        [["1",       0], ["2",           0], ["3",         0], ["4",      0], ["5",           0], ["6",           0], ["7",           0], ["8",          0]],
+        [["p_bush",  0], ["p_surf",      0], ["p_boulder", 0], ["p_rock", 0], ["p_waterfall", 0], ["p_rockywall", 0], ["bike_needed", 0], ["p_trainer",  0]],
     ],
     progress: [
         [["coal_badge", 0], ["forest_badge", 0], ["cobble_badge", 0], ["fen_badge",   0], ["relic_badge", 0], ["mine_badge",   0], ["icicle_badge", 0], ["beacon_badge", 0]],
