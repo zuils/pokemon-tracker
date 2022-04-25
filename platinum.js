b978026d3c2a6662ea94ebf3c474b1756acafef2
Sergio Santana <src.santana@gmail.com>
2022-04-25 21:15:50 +0200

    Added key items to platinum

--- platinum.js
+++ platinum.js
@@ -615,7 +615,7 @@ var platinum = {
         [["coal_badge", 0], ["forest_badge", 0], ["cobble_badge", 0], ["fen_badge",   0], ["relic_badge", 0], ["mine_badge",   0], ["icicle_badge", 0], ["beacon_badge", 0]],
         [["p_aaron",    0], ["p_bertha",     0], ["p_flint",      0], ["p_lucian",    0], ["p_cynthia",   0]],
         [["hm_cut",     0], ["hm_fly",       0], ["hm_surf",      0], ["hm_strength", 0], ["hm_defog",    0], ["hm_rocksmash", 0], ["hm_waterfall", 0], ["hm_rockclimb", 0]],
-        [["bike",       0]],
+        [["bike",       0], ["works_key",    0], ["galactic_key", 0]],
     ],
     obtained: new Set()
 };
